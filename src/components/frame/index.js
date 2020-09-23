ca11fc63df85c45cda13cc2add0d12377ea23518
= <=>
2020-09-23 21:57:54 +0800

    点击导航条使其能够进行相应跳转

--- src/components/frame/index.js
+++ src/components/frame/index.js
@@ -1,32 +1,31 @@
 import React, {Component} from 'react'
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Layout, Menu, Breadcrumb} from 'antd';
 
 import logo from '../../assets/imgs/logo.png'
 import {adminRouteSecond} from '../../routes'
 import {withRouter} from 'react-router-dom'
 import './index.css'
-const { Header, Content,Footer, Sider } = Layout;
 
-const menus = adminRouteSecond.filter(item =>item.isNav)
+const {Header, Content, Footer, Sider} = Layout;
+
+const menus = adminRouteSecond.filter(item => item.isNav)
+
 @withRouter
 class Frame extends Component {
-    state = {
-        collapsed: false,
-    };
-
-    toggle = () => {
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
-    };
+    //切换页面
+    handleNav = ({key}) => {
+        this.props.history.replace(key)
+    }
 
     render() {
-        console.log(this.props)
+        const {pathname} = this.props.location
+        const targetPath =  pathname=== '/admin'?'/admin/dashboard':pathname
+        console.log(pathname)
         return (
             <Layout>
                 <Header className="header">
-                    <div className="logo admin-logo" >
+                    <div className="logo admin-logo">
                         <img src={logo} alt="logo"/>
                     </div>
                 </Header>
@@ -34,11 +33,12 @@ class Frame extends Component {
                     <Sider width={200} className="site-layout-background">
                         <Menu
                             mode="inline"
-                            defaultSelectedKeys={['1']}
-                            style={{ height: '100%', borderRight: 0 }}
+                            defaultSelectedKeys={targetPath}
+                            style={{height: '100%', borderRight: 0}}
+                            onClick={this.handleNav}
                         >
                             {
-                                menus.map(item =>{
+                                menus.map(item => {
                                     return (
                                         <Menu.Item
                                             key={item.pathname}
@@ -52,8 +52,8 @@ class Frame extends Component {
                             }
                         </Menu>
                     </Sider>
-                    <Layout style={{ padding: '0 24px 24px' }}>
-                        <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Layout style={{padding: '0 24px 24px'}}>
+                        <Breadcrumb style={{margin: '16px 0'}}>
                             <Breadcrumb.Item>Home</Breadcrumb.Item>
                             <Breadcrumb.Item>List</Breadcrumb.Item>
                             <Breadcrumb.Item>App</Breadcrumb.Item>
@@ -68,11 +68,12 @@ class Frame extends Component {
                         >
                             {this.props.children}
                         </Content>
-                        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                        <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
                     </Layout>
                 </Layout>
             </Layout>
         )
     }
 }
+
 export default Frame
