a7eb3b52b6c5c782e24ff8aefd0e55980144c7dd
= <=>
2020-09-23 20:40:51 +0800

    修改Craco为react-app-rewired实现按需加载

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
-import {Switch, Route,Redirect} from "react-router-dom";
-
+import {Switch, Route, Redirect} from "react-router-dom";
+import Frame from "./components/frame";
 
 import {adminRouteSecond} from './routes'
-
 export default class App extends React.Component {
     render() {
         return (
-            <Switch>
-                {
-                    adminRouteSecond.map(route => {
-                        return (
-                            <Route path={route.pathname}
-                                   key={route.pathname}
-                                   component={route.component}
-                            />
-                        )
-                    })
-                }
-                <Redirect to={adminRouteSecond[0].pathname} from='/admin' exact />
-                {/*访问其它url，返回404*/}
-                <Redirect to='/404'/>
-            </Switch>
-
+            <Frame>
+                <Switch>
+                    {
+                        adminRouteSecond.map(route => {
+                            return (
+                                <Route path={route.pathname}
+                                       key={route.pathname}
+                                       component={route.component}
+                                />
+                            )
+                        })
+                    }
+                    <Redirect to={adminRouteSecond[0].pathname} from='/admin' exact/>
+                    {/*访问其它url，返回404*/}
+                    <Redirect to='/404'/>
+                </Switch>
+            </Frame>
         )
     }
 }
