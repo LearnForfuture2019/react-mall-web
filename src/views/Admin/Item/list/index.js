a0bca3d4b538db04d06cf4151ed76b8afc161995
= <=>
2020-09-24 18:18:55 +0800

    一点小修改，配置了屏幕自适应

--- src/views/Admin/Item/list/index.js
+++ src/views/Admin/Item/list/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import {Form, Input, Button,  Row, Col,Select,TreeSelect} from 'antd';
+import {Form, Input, Button, Row, Col, Select, TreeSelect,Card} from 'antd';
+
 const {Group} = Button
 const {Option} = Select
 
@@ -44,116 +45,123 @@ export default class Item extends React.Component {
     handleChange = (value) => {
         this.formRef.current.setFieldsValue(value)
     }
+
     render() {
         return (
-            <Form
-                {...layout}
-                name="basic"
-                initialValues={{
-                    remember: true,
-                }}
-                ref={this.formRef}
-                onFinish={this.onFinish}
-                onFinishFailed={this.onFinishFailed}
-                style={{border:'1px solid #dedede',padding:10}}
-            >
-                <Row justify='space-between'>
-                    <Col>
-                        <h3>筛选搜索</h3>
-                    </Col>
-                    <Col>
-                       <Group>
-                           <Button onClick={()=>this.formRef.current.resetFields()}>重置</Button>
-                           <Button type='primary' htmlType="submit">查询结果</Button>
-                       </Group>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={8}>
-                        <Form.Item
-                            label="输入搜索:"
-                            name="inputSearch"
+            <>
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    ref={this.formRef}
+                    onFinish={this.onFinish}
+                    onFinishFailed={this.onFinishFailed}
+                    style={{border: '1px solid #dedede', padding: 10}}
+                >
+                    <Row justify='space-between'>
+                        <Col>
+                            <h3>筛选搜索</h3>
+                        </Col>
+                        <Col>
+                            <Group>
+                                <Button onClick={() => this.formRef.current.resetFields()}>重置</Button>
+                                <Button type='primary' htmlType="submit">查询结果</Button>
+                            </Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xl={8} xs={16}>
+                            <Form.Item
+                                label="输入搜索:"
+                                name="inputSearch"
 
-                        >
-                            <Input placeholder='商品名称'/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="商品货号:"
-                            name="productNumbers"
-
-
-                        >
-                            <Input  placeholder="商品货号"/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="商品分类:"
-                            name="productClassification"
-
-                        >
-                            <TreeSelect
-                                style={{ width: '100%' }}
-                                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                                treeData={treeData}
-                                placeholder="Please select"
-                                treeDefaultExpandAll
-                                onChange={this.onChange}
-                            />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={8}>
-                        <Form.Item
-                            label="商品品牌:"
-                            name="productBrand"
-                        >
-                            <Select
-                                placeholder="请选择品牌"
-                                style={{ width: 120 }}
-                                onChange={this.handleChange}
                             >
-                                <Option value="小米">小米</Option>
-                                <Option value="七匹狼">七匹狼</Option>
-                                <Option value="海澜之家">海澜之家</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="上架状态:"
-                            name="saleState"
-                        >
-                            <Select
-                                placeholder="全部"
-                                style={{ width: 120 }}
-                                onChange={this.handleChange}
+                                <Input placeholder='商品名称'/>
+                            </Form.Item>
+                        </Col>
+                        <Col xl={8} xs={16}>
+                            <Form.Item
+                                label="商品货号:"
+                                name="productNumbers"
+
+
                             >
-                                <Option value="上架">上架</Option>
-                                <Option value="下架">下架</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="审核状态:"
-                            name="auditState"
-                        >
-                            <Select
-                                placeholder="全部"
-                                style={{ width: 120 }}
-                                onChange={this.handleChange}
+                                <Input placeholder="商品货号"/>
+                            </Form.Item>
+                        </Col>
+                        <Col xl={8} xs={16}>
+                            <Form.Item
+                                label="商品分类:"
+                                name="productClassification"
+
                             >
-                                <Option value="审核通过">审核通过</Option>
-                                <Option value="未通过">未通过</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
+                                <TreeSelect
+                                    style={{width: '100%'}}
+                                    dropdownStyle={{maxHeight: 400, overflow: 'auto'}}
+                                    treeData={treeData}
+                                    placeholder="Please select"
+                                    treeDefaultExpandAll
+                                    onChange={this.onChange}
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xl={8} xs={16}>
+                            <Form.Item
+                                label="商品品牌:"
+                                name="productBrand"
+                            >
+                                <Select
+                                    placeholder="请选择品牌"
+                                    style={{width: 120}}
+                                    onChange={this.handleChange}
+                                >
+                                    <Option value="小米">小米</Option>
+                                    <Option value="七匹狼">七匹狼</Option>
+                                    <Option value="海澜之家">海澜之家</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col xl={8} xs={16}>
+                            <Form.Item
+                                label="上架状态:"
+                                name="saleState"
+                            >
+                                <Select
+                                    placeholder="全部"
+                                    style={{width: 120}}
+                                    onChange={this.handleChange}
+                                >
+                                    <Option value="上架">上架</Option>
+                                    <Option value="下架">下架</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col xl={8} xs={16}>
+                            <Form.Item
+                                label="审核状态:"
+                                name="auditState"
+                            >
+                                <Select
+                                    placeholder="全部"
+                                    style={{width: 120}}
+                                    onChange={this.handleChange}
+                                >
+                                    <Option value="审核通过">审核通过</Option>
+                                    <Option value="未通过">未通过</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+                <Card title="数据列表"
+                      extra={<Button>添加</Button>}
+                      style={{ width: '100%' }}>
+                </Card>
+            </>
         )
     }
 }
